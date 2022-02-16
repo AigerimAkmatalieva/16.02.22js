bcefbcd16ad77b15fa5d6ee064a175e9f85ddef9
AigerimAkmatalieva <akmatalieva2108@gmail.com>
2022-02-16 14:54:06 +0600

    added input

--- script.js
+++ script.js
@@ -13,3 +13,13 @@ document.querySelector('input[type="text"]')
 .addEventListener('change', function() {
   alert('Change')
 });
+
+document.querySelector('input[type="checkbox"]') 
+.addEventListener('change', function() {
+  if (this.checked) {
+    alert('You are employed');
+   }
+  else {
+    alert('You are unemployed')
+   }
+});
