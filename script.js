a30bef6e689130aa3552b178d49a6ca811d8c701
AigerimAkmatalieva <akmatalieva2108@gmail.com>
2022-02-16 14:27:27 +0600

    added input

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelector('input[type="text"]') 
+.addEventListener('focus', function() {
+  this.value = "Hello";
+});
+document.querySelector('input[type="text"]') 
+.addEventListener('blur', function() {
+  this.value = "bye";
+})
